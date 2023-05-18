5a6a9964ab0efec897579b89ce47ed2502433d57
DmitryGrigorich <dimamihelson739@gmail.com>
2023-05-19 02:54:38 +0400

    fix: исправить ошибки после ревью

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,11 @@
 import initialCards from "./constants.js";
-import { openPopup, closePopup } from "./utils.js";
+import { openPopup } from "./modal.js";
 
-const popupPlaceAdd = document.querySelector('.popup_type_place');
 const popupImage = document.querySelector('.popup_type_image');
 const popupImagePicture = popupImage.querySelector('.popup__image');
 const cardTemplate = document.querySelector('#template_card').content;
 const cardsContainer = document.querySelector('.cards');
 const imageCaption = popupImage.querySelector('.popup__caption');
-const popupPlace = document.forms.placeForm.placeInput;
-const popupImageInput = document.forms.placeForm.imageInput;
 
 // функция создания карточки
 const createCard = (place, link) => {
@@ -16,6 +13,28 @@ const createCard = (place, link) => {
   card.querySelector('.card__image').src = link;
   card.querySelector('.card__image').alt = place;
   card.querySelector('.card__title').textContent = place;
+  card.addEventListener('click', (evt) => {
+    const evtTarget = evt.target;
+    const evtCurrentTarget = evt.currentTarget;
+    // кнопка лайка
+    if (evtTarget.classList.contains('card__like')) {
+      evtTarget.classList.toggle('card__like_active');
+    }
+    // кнопка удаления
+    if (evtTarget.classList.contains('card__delete')) {
+      evtTarget.closest('.card').remove();
+    }
+    // открытие попапа с картинкой при нажатии на картинку
+    if (evtTarget.classList.contains('card__image')) {
+      const itemText = evtCurrentTarget.querySelector('.card__title').textContent;
+      
+      popupImagePicture.src = evtTarget.src;
+      popupImagePicture.alt = itemText;
+      imageCaption.textContent = itemText;
+      openPopup(popupImage);
+    }
+  });
+
   return card;
 }
 
@@ -30,40 +49,9 @@ const addCardsFromArr = () => {
 
 // обработчик действий в карточке
 const listenerCards = () => {
-  cardsContainer.addEventListener('click', (evt) => {
-    const evtTarget = evt.target;
-  
-    // кнопка лайка
-    if (evtTarget.classList.contains('card__like')) {
-      evtTarget.classList.toggle('card__like_active');
-    }
-    // кнопка удаления
-    if (evtTarget.classList.contains('card__delete')) {
-      evtTarget.closest('.card').remove();
-    }
-    // открытие попапа с картинкой при нажатии на картинку
-    if (evtTarget.classList.contains('card__image')) {
-      const itemText = evtTarget.parentNode.querySelector('.card__title').textContent;
-      
-      popupImagePicture.src = evtTarget.src;
-      popupImagePicture.alt = itemText;
-      imageCaption.textContent = itemText;
-      openPopup(popupImage);
-    }
-  });
 }
 
 
-// добавление карточек через форму
-const submitCardFormHandler = (evt) => {
-  evt.preventDefault();
 
-  const card = createCard(popupPlace.value, popupImageInput.value);
-  cardsContainer.prepend(card);
-    
-  closePopup(popupPlaceAdd);
-    
-  evt.target.reset();
-};
 
-export { addCardsFromArr, listenerCards, submitCardFormHandler };
+export { createCard, addCardsFromArr, listenerCards };
